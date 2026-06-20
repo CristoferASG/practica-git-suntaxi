b2b00468a55e97714d4e396629c739fb6f76f5d2
CristoferASG <cag.suntaxi@yavirac.edu.ec>
2026-06-19 22:41:58 -0500

    Primer commit del proyecto

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def reloj():
+    hora = datetime.now().strftime("%H:%M:%S")
+    return f"""
+    <html>
+    <head>
+        <title>Reloj Flask</title>
+    </head>
+    <body style="font-family:Arial;text-align:center;margin-top:100px;">
+        <h1>Reloj Digital</h1>
+        <h2>{hora}</h2>
+        <p>Aplicación Flask para DevOps</p>
+    </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
